b80a982d8443fbaf13defde115348a8be4c896b9
poyanuna <122661640+poyanuna@users.noreply.github.com>
2023-01-28 17:01:08 +0900

    Create 2739.cpp

---
+++ boj/2739.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i=1;i<10;i++)
+    {
+        cout << n << " " << "*" << " "<< i << " "<< "=" << " "<< n*i << "\n";
+    }
+
+    return 0;
+}
