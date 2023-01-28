97d7a9c6b6c506d84019b89c9b1fae419724887f
poyanuna <122661640+poyanuna@users.noreply.github.com>
2023-01-28 15:39:59 +0900

    Create  10952.cpp

---
+++ boj/10952.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(true)
+    {
+       cin >> a >> b;
+
+
+       if(a==0 && b==0)
+        {
+        break;
+       }
+       cout << a+b << "\n";
+    }
+
+
+
+    return 0;
+}
